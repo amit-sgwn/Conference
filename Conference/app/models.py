92dec3c8ee26f02de9fa937ed166b26c4535f5ac
amit-sgwn <amitsangwan898@gmail.com>
2017-02-01 19:04:05 +0530

    Added model to project

--- Conference/app/models.py
+++ Conference/app/models.py
@@ -5,3 +5,16 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+
+class Speaker(models.Model):
+    name = models.CharField(max_lendth=50)
+    title = models.CharField(max_length=50)
+    bio = models.TextField(max_length=1000)
+
+class Track(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+
+class Session(models.Model):
+    title = models.CharField(max_length=50)
+    abstract = models.TextField(max_length=1000)
