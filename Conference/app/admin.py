4212a4be3158d4d1dda515f4eff173f2253e69d5
amit-sgwn <amitsangwan898@gmail.com>
2017-02-02 13:54:00 +0530

    Added search capability to Session class

--- Conference/app/admin.py
+++ Conference/app/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from app.models import *
 
-admin.site.register(Track)
-admin.site.register(Session)
-admin.site.register(Speaker)
+class TrackAdmin(admin.ModelAdmin):
+    list_display=('title','description',)
+admin.site.register(Track,TrackAdmin)
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('title','track',)
+    search_fields = ['title','abstract']
+
+admin.site.register(Session,SessionAdmin)
+
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ('name','bio',)
+admin.site.register(Speaker,SpeakerAdmin)
