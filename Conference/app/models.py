f98368f17bcfd54b9c6c0e1f29d3b64d5a45545a
amit-sgwn <amitsangwan898@gmail.com>
2017-02-02 13:05:01 +0530

    Added __str__(self) method to models

--- Conference/app/models.py
+++ Conference/app/models.py
@@ -10,13 +10,21 @@ class Speaker(models.Model):
     name = models.CharField(max_length=50)
     title = models.CharField(max_length=50)
     bio = models.TextField(max_length=1000)
+    def __str__(self):
+        return self.name
 
 class Track(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.title
 
 class Session(models.Model):
     title = models.CharField(max_length=50)
     abstract = models.TextField(max_length=1000)
     track = models.ForeignKey(Track)
     speaker = models.ForeignKey(Speaker)
+
+    def __str__(self):
+        return self.title 
